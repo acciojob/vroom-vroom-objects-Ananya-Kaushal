f1f05c7d2b76547231ffb7dcb02cc2b9753a72c9
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2026-02-10 02:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-// Complete the js code
-function Car(make, model) {}
+// 1. Car Constructor
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+// 2. SportsCar Constructor
+function SportsCar(make, model, topSpeed) {
+    // Inherit properties from Car using call
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+// Inherit Car's prototype methods
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Fix the constructor pointer (otherwise it would point to Car)
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
